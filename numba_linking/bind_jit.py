250378b6911e36e958716f886f3866b9e63dec3a
Mikhail Goykhman <mikhailgoykhman@Mikhails-MacBook-Pro-2.local>
2024-11-14 16:18:59 -0500

    refactor

--- numba_linking/bind_jit.py
+++ numba_linking/bind_jit.py
@@ -13,6 +13,10 @@ from numba.experimental.function_type import _get_wrapper_address
 
 _ = ir, intrinsic
 
+PY_SFX = '_py'
+SIG_SFX = '_sig'
+JIT_OPTS_SFX = '_jit_options'
+JIT_SFX = '_jit'
 
 random_name_substr_len = 20
 
@@ -29,33 +33,43 @@ class FuncData(typing.NamedTuple):
     ns: dict
 
 
-PY_SFX = '_py'
-SIG_SFX = '_sig'
-JIT_OPTS_SFX = '_jit_options'
-JIT_SFX = '_jit'
+def check_and_populate_ns(name, obj, ns):
+    look_up = ns.get(name)
+    if look_up is not None:
+        assert look_up == obj, f"Conflict: adding {obj} as {name}, found existing {look_up}"
+    ns[name] = obj
+
+
+def populate_ns(func_py, func_name, sig, jit_options):
+    module = inspect.getmodule(func_py)
+    ns = module.__dict__
+    check_and_populate_ns(f'{func_name}{SIG_SFX}', sig, ns)
+    check_and_populate_ns(f'{func_name}{JIT_OPTS_SFX}', jit_options, ns)
+    check_and_populate_ns(f'{func_name}{PY_SFX}', func_py, ns)
+    check_and_populate_ns('numba', numba, ns)
+    return ns
+
+
+def extract_py_func(func):
+    if isinstance(func, types.FunctionType):
+        return func
+    elif isinstance(func, numba.core.registry.CPUDispatcher):
+        return func.py_func
+    elif isinstance(func, numba.core.ccallback.CFunc):
+        return func._pyfunc
+    else:
+        raise ValueError(f"Unsupported {func} of type {type(func)}")
 
 
 def get_func_data(func, sig, jit_options=None):
-    if jit_options is None:
-        jit_options = {}
-    if isinstance(func, numba.core.registry.CPUDispatcher):
-        func_py = func.py_func
-    elif isinstance(func, numba.core.ccallback.CFunc):
-        func_py = func._pyfunc
-    elif isinstance(func, types.FunctionType):
-        func_py = func
-    else:
-        raise ValueError(f"Unsupported {func} of type {type(func)}")
+    jit_options = {} if jit_options is None else jit_options
+    func_py = extract_py_func(func)
     func_name = f"{func_py.__name__}{random_string(random_name_substr_len)}"
     func_args = inspect.getfullargspec(func_py).args
     func_args_str = ', '.join(func_args)
     func_jit_str = f"{func_name}{JIT_SFX} = numba.njit({func_name}{SIG_SFX}, **{func_name}{JIT_OPTS_SFX})({func_name}{PY_SFX})"  # noqa: E501
     func_jit_code = compile(func_jit_str, inspect.getfile(func_py), mode='exec')
-    module = inspect.getmodule(func_py)
-    ns = module.__dict__
-    ns[f'{func_name}{SIG_SFX}'] = sig
-    ns[f'{func_name}{JIT_OPTS_SFX}'] = jit_options
-    ns[f'{func_name}{PY_SFX}'] = func_py
+    ns = populate_ns(func_py, func_name, sig, jit_options)
     exec(func_jit_code, ns)
     func_p = _get_wrapper_address(ns[f'{func_name}{JIT_SFX}'], sig)
     return FuncData(func_name, func_args_str, func_p, func_py, ns)
@@ -64,7 +78,7 @@ def get_func_data(func, sig, jit_options=None):
 def repr_of_type(ty, ns):
     ty_name = repr(ty)
     if hasattr(numba, ty_name):
-        ns[ty_name] = ty
+        check_and_populate_ns(ty_name, ty, ns)
         return ty_name
     elif isinstance(ty, numba.core.types.StructRef):
         for k, v in ns.items():
@@ -75,7 +89,7 @@ def repr_of_type(ty, ns):
         raise RuntimeError(f"Unknown type {ty}")
 
 
-def populate_ns(ns: typing.Dict):
+def populate_ns_imports(ns: typing.Dict):
     ns['intrinsic'] = intrinsic
     ns['ir'] = ir
     ns['cgutils'] = cgutils
@@ -116,7 +130,7 @@ def bind_jit(sig, **jit_options):
     def wrap(func):
         func_data = get_func_data(func, sig, jit_options)
         ll.add_symbol(func_data.func_name, func_data.func_p)
-        populate_ns(func_data.ns)
+        populate_ns_imports(func_data.ns)
         ret_type = repr_of_type(sig.return_type, func_data.ns)
         arg_types = [repr_of_type(arg, func_data.ns) for arg in sig.args]
         sig_str = f"{ret_type}({', '.join(arg_types)})"
